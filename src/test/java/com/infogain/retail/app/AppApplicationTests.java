e3aa45318c9ac958b06d52efa1e5e0896abeb2dc
wierzbickis <s.wierzbicki.kontakt@gmail.com>
2023-03-24 15:17:02 +0100

    Initial commit

---
+++ src/test/java/com/infogain/retail/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infogain.retail.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
